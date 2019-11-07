d5c7727db6476e265c140b70c85d31a4ed1aebd6
Wei-Tse Hsu <43389323+wehs7661@users.noreply.github.com>
2019-11-06 22:56:20 -0700

    Added README, developed insert and print methods and their corresponding using TDD

--- binary_tree.py
+++ binary_tree.py
@@ -1,8 +1,105 @@
+import sys
+
+
 class Node:
     def __init__(self, key, value=None, left=None, right=None):
+        """
+        Initialization of a node of a tree
+
+        Parameters
+        ----------
+        key : int or float
+            The key of the node
+        value : could be any data type
+            The value corresponding to the key
+        left : obj
+            An object of Node serving as the left child of the node
+        right : obj
+            An object of Node serving as the right child of the node
+        """
+        self.value = value    # which is None here (default)
+        self.left = left      # which is None here (default)
+        self.right = right    # which is None here (default)
+        self.key = key
 
 def insert(root, key, value=None):
-    return root
+    """
+    This function insert a value given a root of a tree or a subtree, 
+    which could be a child of other nodes.
+
+    Parameters
+    ----------
+    root : obj
+        An object of Node serving as a root of a tree or a subtree
+    key : int or float
+        The key to be inserted
+    value : could be any data type
+        The value corresponding to the inserting key
+
+    Return
+    ------
+    None
+    """
+    if root is None:
+        root = Node(key, value)
+    else:
+        if key >= root.key:
+            if root.right is None:
+                root.right = Node(key, value)
+            else:
+                # Use root.right as the root of the subtree
+                insert(root.right, key, value)
+        else:
+            if root.left is None:
+                root.left = Node(key, value)
+            else:
+                # Use root.left as the root of the subtree
+                insert(root.left, key, value)
+
+def print_tree(root, key_list=[], value_list=[]):
+    """
+    This function prints key of each node in a tree to a list, from the 
+    rightmost node to the leftmost. This function could be useful for 
+    checking the functionality of the insert function.
+
+    Parameters
+    ----------
+    root : obj
+        An object of Node serving as a root of a tree or a subtree
+    key_list : list
+        A list of keys
+    value_list : list
+        A list of value
+
+    Returns
+    -------
+    None
+    """
+
+    if root is None:
+        print('The root of the tree is null!')
+        sys.exit()
+    else:
+        key_list.append(root.key)
+        value_list.append(root.value)
+        if root.right is not None:
+            print_tree(root.right, key_list, value_list)
+            # root.right.key will be print out when executing print(root.key)
+            # in print_tree(root.right)
+        if root.left is not None:
+            print_tree(root.left, key_list, value_list)
+            # root.left.key will be print out when executing print(root.key)
+            # in print_tree(root.left)
+            
+    return key_list, value_list
 
 def search(root, key):
-    return None
+    if root is None:
+        return None
+    else:
+        if root.key == key:
+            return root.value
+
+
+
+
---
+++ test_binary_tree.py
@@ -0,0 +1,37 @@
+import binary_tree as bt
+import unittest
+
+class TestBinaryTree(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.root = bt.Node(6, 'Root')
+        bt.insert(cls.root, 7, 'A')
+        bt.insert(cls.root, 8, 'B')
+        bt.insert(cls.root, 9, 'C')
+        bt.insert(cls.root, 3, 'D')
+        bt.insert(cls.root, 2, 'E')
+        bt.insert(cls.root, 4, 'F')
+        bt.insert(cls.root, 5, 'G')
+        #             _6_
+        #            /   \
+        #           3     7
+        #          / \     \
+        #         2   4     8
+        #              \     \
+        #               5     9
+
+    def test_insert(self):
+        self.assertEqual([self.root.key, self.root.value], [6, 'Root'])
+        self.assertEqual([self.root.left.key, self.root.left.value], [3, 'D'])
+        self.assertEqual([self.root.right.key, self.root.right.value], [7, 'A'])
+        self.assertEqual([self.root.left.left.key, self.root.left.left.value], [2, 'E'])
+
+    def test_print_tree(self):
+        key_list, value_list = bt.print_tree(self.root)
+        self.assertEqual(key_list, [6, 7, 8, 9, 3, 4, 5, 2])
+        self.assertEqual(value_list, ['Root', 'A', 'B', 'C', 'D', 'F', 'G', 'E'])
+
+if __name__ == '__main__':
+    unittest.main()
+
+        
